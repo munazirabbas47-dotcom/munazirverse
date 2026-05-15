2948e9eb4cf7d3bd1a9c44336b5f094795c8b7a6
Munazir Abbas <munazirabbas47-dotcom@users.noreply.github.com>
2026-05-15 13:51:51 +0500

    add models for contact

--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+phone_validator = RegexValidator(
+    regex=r'^\d{11,13}$',
+    message="Phone number must contain only numbers and be between 11 to 13 digits."
+)
+
+# create model for contact
+class Contact(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=13,validators=[phone_validator])
+    desc = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return  'Message From ' + self.name + ' - ' + self.email
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Contact
 
 
 # Create your views here.
@@ -8,6 +9,15 @@ def home(request):
     return render(request, "home/index.html")
     
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        phone = request.POST.get("phone", "")
+        desc = request.POST.get("desc","")
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+        thanks = True
+        return render(request, "home/contact.html", {"thanks": thanks})
     return render(request, "home/contact.html")
 
 def about(request):
