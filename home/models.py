1d9725637690b2a483dd54153a88d20a80fdfee2
Munazir Abbas <munazirabbas47-dotcom@users.noreply.github.com>
2026-05-17 11:51:45 +0500

    Fix - model validator

--- home/models.py
+++ home/models.py
@@ -1,22 +1,44 @@
 from django.db import models
 from django.utils import timezone
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, MinLengthValidator
 
 # Create your models here.
 
 phone_validator = RegexValidator(
-    regex=r'^\d{11,13}$',
-    message="Phone number must contain only numbers and be between 11 to 13 digits."
+    regex=r"^\d{11,13}$",
+    message="Phone number must contain only numbers and be between 11 to 13 digits.",
 )
+
 
 # create model for contact
 class Contact(models.Model):
     sno = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    email = models.CharField(max_length=100)
-    phone = models.CharField(max_length=13,validators=[phone_validator])
-    desc = models.TextField()
+    
+    name = models.CharField(
+        max_length=255,
+        validators=[
+            MinLengthValidator(3, message="Name must be at least 3 characters long.")
+        ],
+    )
+
+    email = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(4, message="Email must be at least 4 characters long.")
+        ],
+    )
+
+    phone = models.CharField(max_length=13, validators=[phone_validator])
+
+    desc = models.TextField(
+        validators=[
+            MinLengthValidator(
+                4, message="Description must be at least 4 characters long."
+            )
+        ]
+    )
+
     timestamp = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return  'Message From ' + self.name + ' - ' + self.email
+        return "Message From " + self.name + " - " + self.email
--- home/views.py
+++ home/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 from .models import Contact
 
 
+
 # Create your views here.
 
 def home(request):
     return render(request, "home/index.html")
     
 def contact(request):
+
     if request.method == "POST":
         name = request.POST.get("name", "")
         email = request.POST.get("email", "")
